ab951905e2d890ddef4c1ecf0d1b7581a44a9874
Aleks <kuriatae@mail.ru>
2021-07-28 18:40:36 +0700

    HomeWork_5

--- app/src/main/java/com/example/pictureoftheday/ui/main/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/example/pictureoftheday/ui/main/PictureOfTheDayFragment.kt
@@ -1,13 +1,19 @@
 package com.example.pictureoftheday.ui.main
 
 import android.os.Bundle
+import android.system.Os.bind
 import android.view.View
+import android.view.animation.AnticipateOvershootInterpolator
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.transition.ChangeBounds
+import androidx.transition.TransitionManager
 import coil.api.load
 import com.example.pictureoftheday.R
 import com.example.pictureoftheday.databinding.PictureOfTheDayFragmentBinding
+import com.example.pictureoftheday.databinding.PictureOfTheDayFragmentStartBinding
 import com.example.pictureoftheday.domain.appstate.PictureOfTheDayData
 import com.example.pictureoftheday.domain.observe.PublisherHolder
 import com.example.pictureoftheday.domain.router.AppRouter
@@ -17,26 +23,24 @@ import com.example.pictureoftheday.domain.toast
 import com.example.pictureoftheday.domain.viewBinding
 
 
-class PictureOfTheDayFragment : Fragment(R.layout.picture_of_the_day_fragment) {
+class PictureOfTheDayFragment : Fragment(R.layout.picture_of_the_day_fragment_start) {
 
     companion object {
         fun newInstance() = PictureOfTheDayFragment()
     }
 
 
-
-    private var router : AppRouter? = null
-    private var holder : PublisherHolder? = null
+    private var router: AppRouter? = null
+    private var holder: PublisherHolder? = null
     private var dateSelect = 0;
-    private var titleBottomSheetText: String = ""
-    private var descriptionBottomSheetText: String = ""
+    private var show = false
     private val viewModel: PictureOfTheDayViewModel by lazy {
         ViewModelProvider(this).get(PictureOfTheDayViewModel::class.java)
     }
 
 
-    private val B: PictureOfTheDayFragmentBinding by viewBinding(
-        PictureOfTheDayFragmentBinding::bind
+    private val B: PictureOfTheDayFragmentStartBinding by viewBinding(
+        PictureOfTheDayFragmentStartBinding::bind
     )
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -50,13 +54,10 @@ class PictureOfTheDayFragment : Fragment(R.layout.picture_of_the_day_fragment) {
             holder = activity as PublisherHolder?
         }
 
-        B.apply {
-
-            discriptionChip.setOnClickListener {
-                holder?.getPublisher()?.showBottomSheet()
-            }
-
+        B.imageView.setOnClickListener {
+            if (show) hideComponents() else showComponents()
         }
+
         viewModel.getDataPod(dateSelect).observe(viewLifecycleOwner, Observer
         { renderData(it) })
     }
@@ -68,16 +69,11 @@ class PictureOfTheDayFragment : Fragment(R.layout.picture_of_the_day_fragment) {
                 B.apply {
                     progressBar.visibility = View.GONE
                     tapToScreen.visibility = View.VISIBLE
-                    group.visibility = View.VISIBLE
+                    titlePictureDay.visibility = View.VISIBLE
+                    description.visibility = View.VISIBLE
+                    title.text = data.serverResponseData.title.toString()
+                    description.text = data.serverResponseData.explanation.toString()
                 }
-
-                titleBottomSheetText = data.serverResponseData.title.toString()
-                descriptionBottomSheetText = data.serverResponseData.explanation.toString()
-
-                holder?.getPublisher()?.notify(
-                    titleBottomSheetText,
-                    descriptionBottomSheetText
-                )
 
                 val serverResponseData = data.serverResponseData
                 val url = serverResponseData.url
@@ -91,12 +87,15 @@ class PictureOfTheDayFragment : Fragment(R.layout.picture_of_the_day_fragment) {
                         placeholder(R.drawable.ic_no_photo_vector)
                     }
                 }
+
+
             }
             is PictureOfTheDayData.Loading -> {
                 B.apply {
-                    group.visibility = View.GONE
                     progressBar.visibility = View.VISIBLE
                     tapToScreen.visibility = View.GONE
+                    titlePictureDay.visibility = View.GONE
+                    description.visibility = View.GONE
                 }
 
             }
@@ -107,5 +106,34 @@ class PictureOfTheDayFragment : Fragment(R.layout.picture_of_the_day_fragment) {
                 ) { viewModel.getDataPod(dateSelect) }
             }
         }
+    }
+
+    private fun showComponents() {
+
+        ConstraintSet().apply {
+            clone(context, R.layout.picture_of_the_day_fragment)
+            ChangeBounds().apply {
+                interpolator = AnticipateOvershootInterpolator(2.0f)
+                duration = 1200
+                TransitionManager.beginDelayedTransition(B.main, this)
+            }
+            applyTo(B.main)
+            show = true
+        }
+    }
+
+    private fun hideComponents() {
+
+        ConstraintSet().apply {
+            clone(context, R.layout.picture_of_the_day_fragment_start)
+            ChangeBounds().apply {
+                interpolator = AnticipateOvershootInterpolator(0.5f)
+                duration = 600
+                TransitionManager.beginDelayedTransition(B.main, this)
+            }
+            applyTo(B.main)
+            show = false
+        }
+
     }
 }
